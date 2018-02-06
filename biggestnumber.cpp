0ba0cefc5d89746669f1b9c0f0dbac0083600ef1
Maxwell A Palmer <mpalmer16@mail.csuchico.edu>
2018-02-05 17:42:17 -0800

    Compares numbers to determine the largest

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -7,11 +7,21 @@ int main()
 {
   int num1;
   int num2;
+  int biggest;
   cout<<"Please enter a whole number:\n";
   cin>> num1;
   cout<<"Please enter another whole number:\n";
   cin>> num2;
+  if (num1 > num2)
+  {
+  biggest = num1;
+  }
+  else
+  {
+  biggest = num2;
+  }
   cout<<"Of those two numbers, the biggest is: ";
+  cout<< biggest;
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
