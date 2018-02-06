4d9b695d907edc0dc80d4c16d19e8a48023b1366
Maxwell A Palmer <mpalmer16@mail.csuchico.edu>
2018-02-05 17:25:26 -0800

    asks for two whole numbers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,15 +1,16 @@
-//Author:
+//Author: Maxwell Palmer
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+  int num1;
+  int num2;
   cout<<"Please enter a whole number:\n";
-  
+  cin>> num1;
   cout<<"Please enter another whole number:\n";
-
+  cin>> num2;
   cout<<"Of those two numbers, the biggest is: ";
   cout<<endl<<"Thank you for playing.\n";
 
